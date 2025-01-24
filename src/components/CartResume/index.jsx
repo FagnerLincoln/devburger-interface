f8efebc75cbdd6c1054ba3305098a7d7c3241fb7
fagnerlincoln <fagnerlincoln@outlook.com>
2025-01-23 22:48:09 -0300

    reparo

--- src/components/CartResume/index.jsx
+++ src/components/CartResume/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
+
 import { useCart } from "../../hooks/CartContext";
 import { api } from "../../services/api";
 import { formatPrice } from "../../utils/formatPrice";
